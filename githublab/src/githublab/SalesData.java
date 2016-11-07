476713fd9750b8058f0f294931188d8f2a02c33d
td02675 <td02675@georgiasouthern.edu>
2016-11-06 21:00:28 -0500

    output finished

--- githublab/src/githublab/SalesData.java
+++ githublab/src/githublab/SalesData.java
@@ -4,7 +4,7 @@ public class SalesData {
 
 	   public void display() {
 	       for(int i = 0; i < data.length; i++) {
-	    	   System.out.println("");
+	    	   System.out.println("Next Value: "+data[i]);
 	       }
 	   } //End display
 
